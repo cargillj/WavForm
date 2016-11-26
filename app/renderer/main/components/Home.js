3caeaa62948b81fb76932a411685dbcb065217ac
patsul12 <patsul12@gmail.com>
2016-11-25 20:06:56 -0600

    add react audio player, get ref function for audio control, test local audio file currently in place, will not work on pulled repos for now.

--- app/renderer/main/components/Home.js
+++ app/renderer/main/components/Home.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.css';
+import ReactAudioPlayer from 'react-audio-player';
 
 
 export default class Home extends Component {
@@ -10,6 +11,14 @@ export default class Home extends Component {
       <div>
         <div className={styles.container}>
           <h1>Welcome to WavForm</h1>
+          <ReactAudioPlayer
+            src="../../../../../../Downloads/Dan Carlins Hardcore History/dchha01 Alexander versus Hitler.mp3"
+            ref={c => { 
+                this.rap = c
+                this.rap.audioEl.volume = 0.3
+              }
+            }
+          />
         </div>
       </div>
     );
